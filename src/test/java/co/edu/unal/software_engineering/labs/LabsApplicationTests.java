7319d562b3af59c8a9e302173d6a963ba8e62a8d
Vanessa <jvtrivinog@unal.edu.co>
2020-06-08 00:06:32 -0500

    :poop: Error

--- src/test/java/co/edu/unal/software_engineering/labs/LabsApplicationTests.java
+++ src/test/java/co/edu/unal/software_engineering/labs/LabsApplicationTests.java
@@ -1,5 +1,7 @@
 package co.edu.unal.software_engineering.labs;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +13,7 @@ public class LabsApplicationTests{
 
 	@Test
 	public void contextLoads( ){
-		LabsApplication.main( new String[]{ } );
+		assertEquals( "success", "error" );
 	}
 
 }
